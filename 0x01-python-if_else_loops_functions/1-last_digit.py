c6c033e54552e5867f49a4f434fb2db9e6c3d496
Zenitugo <110994907+Zenitugo@users.noreply.github.com>
2022-11-30 01:27:25 +0100

    Complete the source code in order to print the last digit of the number stored in the variable number.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,4 +7,4 @@ if digit > 5:
 elif digit == 0:
     print(f"Last digit of {number:d} is {digit:d} and is 0")
 else:
-    print(f"Last digit of {number:d} is {digit:d} and s less than 6 and not 0")
+    print(f"Last digit of {number:d} is {digit:d} and is less than 6 and not 0")
