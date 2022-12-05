ab3ccdc340927b8f37e79dba766d81a87ce28443
Zenitugo <110994907+Zenitugo@users.noreply.github.com>
2022-12-05 13:47:13 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        if i == 8:
+        if i == 9:
+            break
             print(f"({i}{j})" .format(i, j))
         else:
             print(f"({i}{j})", end=", ")
