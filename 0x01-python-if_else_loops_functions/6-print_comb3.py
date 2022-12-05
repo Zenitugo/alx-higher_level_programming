f294b4aa21319d945b4de7e466b410de32610275
Zenitugo <110994907+Zenitugo@users.noreply.github.com>
2022-12-05 14:04:31 +0100

    Write a program that prints all possible different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for i in range(0, 9):
     for j in range(i + 1, 10):
         if i == 9:
             break
-            print(f"{i}{j}" .format(i, j), end= ", ")
-     print("{:d}" .format(89))   
+            print(f"{i}{j}" .format(i, j))
+            print("{:d}" .format(89), end= ", ")   
