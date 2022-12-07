98051f1f87cdaabdfb9d15aa36aa8e634370d06a
Zenitugo <110994907+Zenitugo@users.noreply.github.com>
2022-12-07 16:00:44 +0100

    Write a function that retrieves an element from a list like in C

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+  for i in range(my_list):
+    if idx < 0:
+      return none
+    elif idx >= len(my_list):
+      return none
+    else:
+      return my_list.pop(idx)
+      print("Element at index {:d} is { }". format(idx, element_at(my_list, idx))
