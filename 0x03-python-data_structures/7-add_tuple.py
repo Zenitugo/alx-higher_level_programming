e1a66ae014e8bebaa33f3d6013d2e238bacd8e00
Zenitugo <110994907+Zenitugo@users.noreply.github.com>
2022-12-09 13:59:36 +0100

    Write a function that adds 2 tuples.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,6 +3,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
   new_tuple = (sum1, sum2)
   sum1 = 0
   sum2 = 0
-  sum1 = tuple_a[0] + tuple_b[0]
-  sum2 = tuple_a[1] + tuple_b[1]
+  t1 = tuple_a + (0, 0)
+  t2 = tuple_b + (0, 0)
+  sum1 = t1[0] + t2[0]
+  sum2 = t1[1] + t2[1]
   return (new_tuple)
